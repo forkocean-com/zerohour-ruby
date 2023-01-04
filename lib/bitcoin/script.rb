c8da6ec2935ed6f415d380ae93ebdff9f5645bfd
forkocean-com <forkocean.com@gmail.com>
2023-01-04 23:11:46 +0300

    Token Test

--- lib/bitcoin/script.rb
+++ lib/bitcoin/script.rb
@@ -820,14 +820,9 @@ class Bitcoin::Script
   # for tokens
   def self.to_call_script(data)
     raw_script = data.split(" ")
-    a0 = [raw_script[0]].pack("H*")
-    a1 = [raw_script[1]].pack("H*")
-    a3 = [raw_script[3]].pack("H*")
-    a4 = [raw_script[4]].pack("H*")
-    a5 = [raw_script[5]].pack("H*")
-    a6 = pack_pushdata(["c2"].pack("H*"))
+    a6 = from_string("c2")
     #out_data << a0 << a1 << a2 << a3 << a4 << a5 << a6
-    to_witness_script(0, a6)
+    pack_pushdata(a6)
   end
 
   # generate multisig output script for given +pubkeys+, expecting +m+ signatures.
