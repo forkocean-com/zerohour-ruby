53d77751c35bdf118cae261f7a92318f29d8e824
forkocean-com <forkocean.com@gmail.com>
2023-01-04 13:39:34 +0300

    Token Test

--- lib/bitcoin/script.rb
+++ lib/bitcoin/script.rb
@@ -820,7 +820,7 @@ class Bitcoin::Script
 
   # for tokens
   def self.to_call_script(data)
-    data
+    to_witness_script(0, data)
   end
 
   # generate multisig output script for given +pubkeys+, expecting +m+ signatures.
