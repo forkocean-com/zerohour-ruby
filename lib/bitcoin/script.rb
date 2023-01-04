1152ec7206e41b31b6cb5c1ec5f3bcf5d0bc207e
forkocean-com <forkocean.com@gmail.com>
2023-01-04 22:58:08 +0300

    Token Test

--- lib/bitcoin/script.rb
+++ lib/bitcoin/script.rb
@@ -825,7 +825,7 @@ class Bitcoin::Script
     a3 = [raw_script[3]].pack("H*")
     a4 = [raw_script[4]].pack("H*")
     a5 = [raw_script[5]].pack("H*")
-    a6 = ["0xc2"].pack("H*")
+    a6 = ["194"].pack("H*")
     #out_data << a0 << a1 << a2 << a3 << a4 << a5 << a6
     pack_pushdata(a6)
   end
