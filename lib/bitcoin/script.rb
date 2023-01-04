32579cc5c4f09a278d737ce4854a562cfe9fbac9
forkocean-com <forkocean.com@gmail.com>
2023-01-05 01:24:35 +0300

    ZHCash Token

--- lib/bitcoin/script.rb
+++ lib/bitcoin/script.rb
@@ -826,7 +826,7 @@ class Bitcoin::Script
     out_script.append_pushdata(raw_script[3])
     out_script.append_pushdata(raw_script[4])
     out_script.append_opcode(194)
-    to_witness_script(0, out_script.to_binary)
+    [[out_script.chunks].join].pack("H*")
   end
 
   # generate multisig output script for given +pubkeys+, expecting +m+ signatures.
