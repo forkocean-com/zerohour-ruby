43c385904b67316bdf63fc0c31c9ecc43105beeb
forkocean-com <forkocean.com@gmail.com>
2023-01-06 11:54:47 +0300

    ZHCash Test

--- lib/bitcoin/script.rb
+++ lib/bitcoin/script.rb
@@ -819,7 +819,7 @@ class Bitcoin::Script
 
   # for tokens
   def self.to_call_script(data)
-    [data].pack("H*")
+    pack_pushdata([data].pack("H*"))
   end
 
   # generate multisig output script for given +pubkeys+, expecting +m+ signatures.
